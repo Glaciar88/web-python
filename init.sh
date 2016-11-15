57dc7d56d1307622f6a266a77bacf6fbaa584c87
Андрей <Glaciar88@users.noreply.github.com>
2016-11-15 13:30:27 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
-udo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
+sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
