f93814fd7f5091855303e2731bf359b73df1c7e9
Glaciar88 <yandukin@hotmail.com>
2016-11-16 11:43:23 +0300

    Джанго проект. Старт

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/ask.py   /etc/gunicorn.d/ask
 sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello
 sudo /etc/init.d/gunicorn restart
