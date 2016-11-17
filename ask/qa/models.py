027e815a368caa77b0c900c8f5fc9545149222c3
Glaciar88 <yandukin@hotmail.com>
2016-11-17 16:49:01 +0300

    Django+MySQL

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,26 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class QuestionManager(models.Manager):
+        def new (self):
+                return super(QuestionManager, self).new().all().reverse()
+        def popular (self):
+                 return super(QuestionManager, self).popular().order_by('popular')
+
+class Question (models.Model):
+	title = models.CharField(max_length = 255)
+	text = models.TextField()
+	added_at = models.DateField()
+	rating = models.IntegerField()
+	author = models.ForeignKey(User, related_name = 'question_author')
+	likes = models.ManyToManyField(User, related_name = 'question_likes')
+	question_objects = QuestionManager();
+
+class Answer (models.Model):
+	text = models.TextField()
+	added_at = models.DateField()
+	question = models.ForeignKey('Question')
+	author = models.ForeignKey(User, related_name = 'answer_author')
+ 
--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/mysql start
 sudo ln -s /home/box/web/etc/ask.py   /etc/gunicorn.d/ask
 sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello
 sudo /etc/init.d/gunicorn restart
