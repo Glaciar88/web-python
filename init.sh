b291a5b42a840ab07b6036d26d3c3ced8b0a3940
Андрей <Glaciar88@users.noreply.github.com>
2016-11-19 08:16:28 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
+sudo /etc/init.d/mysql start
 #sudo mysql -uroot -e 'CREATE DATABASE IF NOT EXISTS db_ask DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci'
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo /etc/init.d/mysql start
 sudo ln -s /home/box/web/etc/ask.py   /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
